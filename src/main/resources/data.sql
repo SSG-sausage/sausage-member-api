97de82306e6f3a61d2175e7b43cef469717a6c4c
JeongSangByuk <jsb100800@naver.com>
2023-01-31 14:45:56 +0900

    [Chore] data.sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,7 @@ VALUES (2, 'bbb', '공혁준', 'bbb', '01033334444', 'testtest', '2023-01-11 19:
         '2023-01-11 19:50:28', 2);
 INSERT INTO member.MBR (MBR_ID, MBR_LOGIN_ID, MBR_NM, MBR_PWD, MBR_HPNO, MBR_IMG_URL, REG_DTS,
                         REGPE_ID, MOD_DTS, MODPE_ID)
-VALUES (3, 'ccc', '전미림,', 'ccc', '01033334444', 'testtest', '2023-01-11 19:50:28', 3,
+VALUES (3, 'ccc', '전미림', 'ccc', '01033334444', 'testtest', '2023-01-11 19:50:28', 3,
         '2023-01-11 19:50:28', 3);
 INSERT INTO member.MBR (MBR_ID, MBR_LOGIN_ID, MBR_NM, MBR_PWD, MBR_HPNO, MBR_IMG_URL, REG_DTS,
                         REGPE_ID, MOD_DTS, MODPE_ID)
